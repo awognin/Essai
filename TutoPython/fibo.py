b20cd87ce130c139a16bbc818361090ba6d33ca7
awognin <awognin@gmail.com>
2017-11-05 09:11:30 +0100

    suite test 6

--- TutoPython/fibo.py
+++ TutoPython/fibo.py
@@ -9,12 +9,12 @@ def fib(n):    # write Fibonacci series up to n
     print a, b, n
 
 def fib2(n):   # return Fibonacci series up to n
-    result = []
+    result = ()
     a, b = 0, 1
     while b < n:
         result.append(b)
         a, b = b, a + b
-        print b
+        print b, a
     return result
     
     
